882ae493d2cb49b9b6590f630fc32fd3199330c8
Mahesh Ruparel <maheshruparel@gmail.com>
2020-10-23 01:35:31 +0530

    Update removeTabs.php

--- Library/removeTabs.php
+++ Library/removeTabs.php
@@ -1,1 +1,15 @@
+// File : functions.php
 
+// Remove WooCommerce Tabs - this code removes all 3 tabs - to be more specific just remove actual unset lines 
+
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 10 );
+
+function woo_remove_product_tabs( $tabs ) {
+
+    unset( $tabs['description'] );    // Remove the description tab
+    unset( $tabs['reviews'] )   		  // Remove the reviews tab
+    unset( $tabs['any_other'] );  // Remove the any other tab
+
+    return $tabs;
+
+}
